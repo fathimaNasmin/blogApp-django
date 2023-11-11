7fd199cfcbde962e094089a47ac808d81ff83375
Fathima nasmin <fathimanesmi@gmail.com>
2023-11-11 16:09:01 -0500

    remove the s3 and migrate to sqlite

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'dotenv',
-    'storages',
     'django.contrib.humanize',
 ]
 
@@ -65,14 +64,22 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blogApp',
+#         'USER': os.getenv('AWS_RDS_USERNAME'),
+#         'PASSWORD': os.getenv('AWS_RDS_PASSWORD'),
+#         'HOST': 'blogapp-db-instance.ceh3lgs20oiy.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
+#  https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogApp',
-        'USER': os.getenv('AWS_RDS_USERNAME'),
-        'PASSWORD': os.getenv('AWS_RDS_PASSWORD'),
-        'HOST': 'blogapp-db-instance.ceh3lgs20oiy.us-east-1.rds.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -124,21 +131,28 @@ MESSAGE_TAGS = {
 
 # S3 BUCKET CONFIG
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_QUERYSTRING_AUTH = False
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_QUERYSTRING_AUTH = False
 
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
--- user/models.py
+++ user/models.py
@@ -2,24 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 from PIL import Image
-from django.core.files.storage import default_storage as storage
-from storages.backends.s3boto3 import S3Boto3Storage
 
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(storage=S3Boto3Storage(), default='./media/default.png', upload_to='media/profile_images', null=True, blank=True)
+    profile_image = models.ImageField(
+        default='default.png', upload_to='profile_images', null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
+      
+    # Resize the image when a user upload
     def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-        image_read = storage.open(self.profile_image.name, "rb")
-        img = Image.open(image_read)
+        super().save()
+
+        img = Image.open(self.profile_image.path)  # Open image
+
+        # resize image
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
-            img.thumbnail(output_size)
-            # img.save(self.profile_image.name, force_insert=True)
-            img.save(image_read, force_insert=True)
+            img.thumbnail(output_size)  # Resize image
+            # Save it again and override the larger image
+            img.save(self.profile_image.path)
