c076935e794785df4a4db4dacae8318ed061f03f
Fathima nasmin <fathimanesmi@gmail.com>
2023-12-05 15:43:28 -0500

    added new field to subscriber model

--- newsletter/models.py
+++ newsletter/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Subscriber(models.Model):
     """Model for Subscriber Emails"""
     email = models.EmailField(max_length=254)
+    subscribed_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = "Subscribers"
